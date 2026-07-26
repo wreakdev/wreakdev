29523602c2f22f8275dd3c4e476fb49261e2f860
WreakDev <217353155+wreakdev@users.noreply.github.com>
2026-07-26 11:05:17 +0200

    Remove: offsec

--- app/page.tsx
+++ app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
           />
           <SkillRow
             category="Cyber Security"
-            skills="TryHackMe (OffSec)"
+            skills="TryHackMe"
             level="[ 2024 - Present ]"
           />
         </section>
